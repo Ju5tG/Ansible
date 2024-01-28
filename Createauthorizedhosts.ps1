4c34c964fbad928a6d86bdf8dca768eb91d10ef9
Ju5tG <134755669+Ju5tG@users.noreply.github.com>
2024-01-28 00:14:32 -0600

    Create Createauthorizedhosts.ps1
    Creates authorized hosts file and sets perms

---
+++ Createauthorizedhosts.ps1
@@ -0,0 +1,2 @@
+New-Item -Type File -Path C:\ProgramData\ssh\administrators_authorized_keys
+icacls.exe "C:\ProgramData\ssh\administrators_authorized_keys" /inheritance:r /grant "Administrators:F" /grant "SYSTEM:F"
