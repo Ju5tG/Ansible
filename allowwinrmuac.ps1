29e4fcee85e9b01c0a3b6668f5408861729a29dd
Ju5tG <134755669+Ju5tG@users.noreply.github.com>
2024-01-28 01:19:54 -0600

    Create allowwinrmuac.ps1

---
+++ allowwinrmuac.ps1
@@ -0,0 +1,8 @@
+$newItemParams = @{
+    Path         = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System'
+    Name         = 'LocalAccountTokenFilterPolicy'
+    Value        = 1
+    PropertyType = 'DWORD'
+    Force        = $true
+}
+$null = New-ItemProperty @newItemParams
