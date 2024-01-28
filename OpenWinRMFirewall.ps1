15118320b4d596cf587aec6dc9a55636750f618f
Ju5tG <134755669+Ju5tG@users.noreply.github.com>
2024-01-28 01:21:17 -0600

    Create OpenWinRMFirewall.ps1

---
+++ OpenWinRMFirewall.ps1
@@ -0,0 +1,16 @@
+#region Ensure WinRM 5986 is open on the firewall
+ $ruleDisplayName = 'Windows Remote Management (HTTPS-In)'
+ if (-not (Get-NetFirewallRule -DisplayName $ruleDisplayName -ErrorAction Ignore)) {
+     $newRuleParams = @{
+         DisplayName   = $ruleDisplayName
+         Direction     = 'Inbound'
+         LocalPort     = 5986
+         RemoteAddress = 'Any'
+         Protocol      = 'TCP'
+         Action        = 'Allow'
+         Enabled       = 'True'
+         Group         = 'Windows Remote Management'
+     }
+     $null = New-NetFirewallRule @newRuleParams
+ }
+ #endregion
