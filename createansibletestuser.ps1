dc62a637a3e087ad411b0fa273c4b7e97f01807c
Ju5tG <134755669+Ju5tG@users.noreply.github.com>
2024-01-28 01:10:00 -0600

    Create createansibletestuser.ps1

---
+++ createansibletestuser.ps1
@@ -0,0 +1,11 @@
+$testUserAccountName = 'ansibletestuser'
+$testUserAccountPassword = (ConvertTo-SecureString -String 'p@$$w0rd12' -AsPlainText -Force)
+if (-not (Get-LocalUser -Name $testUserAccountName -ErrorAction Ignore)) {
+    $newUserParams = @{
+        Name                 = $testUserAccountName
+        AccountNeverExpires  = $true
+        PasswordNeverExpires = $true
+        Password             = $testUserAccountPassword
+    }
+    $null = New-LocalUser @newUserParams
+}
